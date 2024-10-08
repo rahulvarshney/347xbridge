869f3f5580d8ce0275c8b8de74b04e115cae488d
rahulvarshney <rahulvarshney@gmail.com>
2024-10-08 10:55:08 -0700

    0.0.8

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   const count = useSignal(3);
   return (
     <div class="px-1 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-row items-center justify-center">
+      <div class="flex flex-row">
         <img
           class="my-6"
           src="/logo.svg"
