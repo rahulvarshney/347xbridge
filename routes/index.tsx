a2fccc4780e4c376d12706489503656e519e1e2d
rahulvarshney <rahulvarshney@gmail.com>
2024-10-07 20:45:20 -0700

    0.0.1

---
+++ routes/index.tsx
@@ -0,0 +1,37 @@
+/*import { useSignal } from "@preact/signals";
+import Counter from "../islands/Counter.tsx";
+
+export default function Home() {
+  const count = useSignal(3);
+  return (
+    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+        <img
+          class="my-6"
+          src="/logo.svg"
+          width="128"
+          height="128"
+          alt="the Fresh logo: a sliced lemon dripping with juice"
+        />
+        <h1 class="text-4xl font-bold">Welcome to 347 xbridge going STATIC</h1>
+        <p class="my-4">
+          Try updating this message in the
+          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+        </p>
+        <Counter count={count} />
+      </div>
+    </div>
+  );
+}
+*/
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+const HTML = await Deno.readFile("./static/index.html");
+
+serve(async () => {
+  return new Response(HTML, {
+    headers: {
+      "content-type": "text/html",
+    },
+  });
+});
