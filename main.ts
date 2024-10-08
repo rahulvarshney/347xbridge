e04358ab1ed256b565dbebcb83ed6100da8f4b21
rahulvarshney <rahulvarshney@gmail.com>
2024-10-08 06:40:22 -0700

    0.0.2

--- main.ts
+++ main.ts
@@ -4,10 +4,23 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
-import "$std/dotenv/load.ts";
+/*import "$std/dotenv/load.ts";
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
 
 await start(manifest, config);
+*/
+
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+const HTML = await Deno.readFile("./static/index.html");
+
+serve(async () => {
+  return new Response(HTML, {
+    headers: {
+      "content-type": "text/html",
+    },
+  });
+});
