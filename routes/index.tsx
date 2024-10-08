5f5f3e520afcbb6669cf079d7852f4f8362f708e
rahulvarshney <rahulvarshney@gmail.com>
2024-10-08 16:55:14 -0700

    grid

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,31 +4,23 @@ import Counter from "../islands/Counter.tsx";
 export default function Home() {
   const count = useSignal(3);
   return (
-    <div class="px-1 py-8 mx-auto bg-[#86efac]">
-      <div class="flex flex-wrap">
+    <div class="px-1 py-8 mx-auto bg-[#86efac] grid grid-cols-4 gap-4">
+        <div class="basis-1/3">Points</div>
+        <div class="basis-2/3">13-21</div>
+        <div class="basis-2/3">Bid</div>
+        <div>Restrictions/Comments/Rebids</div>
+        <div>1 of a suit</div>
+        <div>&ge; 5 card major suit; If no 5 card major then bid longer minor (if 3-3 bid 1C: if 4-4 bid 1d)
 
-        <div class="flex flex-wrap">
-        <p>Points BI</p>
-        <p>13-21</p>
-        <p class="grow">Bid</p>
-        <p>Restrictions/Comments/Rebids</p>
-        <p>1 of a suit</p>
-        <p>&ge; 5 card major suit; If no 5 card major then bid longer minor (if 3-3 bid 1C: if 4-4 bid 1d)</p>
         </div>
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">WRAP</h1>
-        <p class="my-4">
+
+        <div class="text-4xl font-bold">WRAP</div>
+        <div class="my-4">
           Try updating this message in the OOH YEAH SOME LIKE IT RW
           <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
+        </div>
         <Counter count={count} />
-      </div>
+      
     </div>
   );
 }
