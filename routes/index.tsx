23009581bcdeacc2cdc338a24c149d1824d90679
rahulvarshney <rahulvarshney@gmail.com>
2024-10-13 20:35:12 -0700

    responsive

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,17 +5,17 @@ export default function Home() {
   const count = useSignal(3);
   return (
 <div class="grid">
-  <span>
+  <span class="xpoints">
     <strong>Points</strong>
   </span>
-  <span>
+  <span class="xbid">
     <strong>Bid</strong>
   </span>  
-  <span>
+  <span class="xrcr">
     <strong>Restrictions/Comments/Rebids</strong>
   </span>
-  <span>13-21</span>
-  <span>1 of a suit</span>
+  <span class="xpointsvalue">13-21</span>
+  <span class="xbidvalue">1 of a suit</span>
   <span>&ge; 5 card major suit; If no 5 card major then bid longer minor 
           (If 3-3, bid 1C; if 4-4 then bid 1D)
   </span>
@@ -24,10 +24,10 @@ export default function Home() {
     in your two longest suits to your HCP. 
     If the total is 20 or more make a normal opening bid of 1 of a suit.</span>
     <span>
-      <p>&bull; Use this rule only on first and second seat.
+      <p>&#10086;Use this rule only on first and second seat.
       (that means your partner has not yet had a chance to bid)
       </p>
-      <p>&bull; Other restrictions as above.</p>
+      <p>&#10086; Other restrictions as above.</p>
     </span>
 </div>
 
