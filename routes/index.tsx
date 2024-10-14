8ad758da7cf90e48adc8670fb57b6130b491dd1a
rahulvarshney <rahulvarshney@gmail.com>
2024-10-13 21:19:31 -0700

    first item

--- routes/index.tsx
+++ routes/index.tsx
@@ -16,14 +16,14 @@ export default function Home() {
   </span>
   <span class="xpointsvalue">13-21</span>
   <span class="xbidvalue">1 of a suit</span>
-  <span>&ge; 5 card major suit; If no 5 card major then bid longer minor 
+  <span class="xrcrvalue">&ge; 5 card major suit; If no 5 card major then bid longer minor 
           (If 3-3, bid 1C; if 4-4 then bid 1D)
   </span>
-  <span>Rule of 20</span>
-  <span>For weaker hands, add the number of cards
+  <span class="xpointsvalue">Rule of 20</span>
+  <span class="xbidvalue">For weaker hands, add the number of cards
     in your two longest suits to your HCP. 
     If the total is 20 or more make a normal opening bid of 1 of a suit.</span>
-    <span>
+    <span class="xrcrvalue">
       <p>&#10086;Use this rule only on first and second seat.
       (that means your partner has not yet had a chance to bid)
       </p>
